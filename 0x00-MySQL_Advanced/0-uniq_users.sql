ae11f78b1a336c4a3bdc2d5fb255224d2cb9d263
omaks45 <danielgozie5@gmail.com>
2023-05-07 20:58:07 +0000

    0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- A script that create a table called users
 -- has id, email and name as requirements
 
-CREATE TABLE IF NOT EXITS users(
+CREATE TABLE IF NOT EXISTS users(
 	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email varchar(255) NOT NULL UNIQUE,
 	name varchar(255)
