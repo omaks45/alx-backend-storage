1802ae3a66e10cc877a22bd45be268a4fbcb0bfc
omaks45 <danielgozie5@gmail.com>
2023-05-10 00:09:49 +0000

    9-index_name_score.sql

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
 -- create indexes of two columns
 -- only their first letter should be indexed
-CREATE INDEX idx_name_first_score ON names(name(1), score(1));
+CREATE INDEX idx_name_first_score ON names(name(1), score);
