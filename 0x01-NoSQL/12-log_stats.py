8adbaade918d45c250ca738c02db7b89e9114c63
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-05-13 04:38:18 +0100

    12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -10,16 +10,16 @@ def log_stats():
     """ Database: logs
     """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_stat = client.logs.nginx
-    agg_counts = logs_stat.count_documents({})
-    get = logs_stat.count_documents({"method": "GET"})
-    post = logs_stat.count_documents({"method": "POST"})
-    put = logs_stat.count_documents({"method": "PUT"})
-    patch = logs_stat.count_documents({"method": "PATCH"})
-    delete = logs_stat.count_documents({"method": "DELETE"})
-    path = logs_stat.count_documents(
+    logs_collection = client.logs.nginx
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    path = logs_collection.count_documents(
         {"method": "GET", "path": "/status"})
-    print(f"{agg_counts} logs")
+    print(f"{total} logs")
     print("Methods:")
     print(f"\tmethod GET: {get}")
     print(f"\tmethod POST: {post}")
